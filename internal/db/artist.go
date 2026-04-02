5a9c81adcf5e2aa0f7c2b7ee1dfe78293b9679da
Guillaume GILLET <mailgpif@gmail.com>
2026-04-02 21:32:19 +0200

    feat: ajouter le fichier artist.go dans le package db

---
+++ internal/db/artist.go
@@ -0,0 +1,10 @@
+package db
+
+import "gorm.io/gorm"
+
+type Artist struct {
+	gorm.Model
+	ID            uint `gorm:"primaryKey;autoIncrement:true"`
+	Name          string
+	MusicBrainzID *string
+}
